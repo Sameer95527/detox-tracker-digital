5ad94a7f1a14cd6d922aad5306042c2ff9e0455d
Sameer95527 <sksameergnt@gmail.com>
2025-08-01 21:34:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('✅ MongoDB Connected');
   app.listen(PORT, () => {
